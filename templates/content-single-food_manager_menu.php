36817e815c64688bec615e2c4f7aae07bb3b3ac3
Kushang Tailor <kushangdarji78@gmail.com>
2022-10-06 18:01:34 +0530

    #76 Design the front view of Food menu.
    Issue: 76
    **********
    Food Menu V2
    
    Issue: 76 : Design the front view of Food menu
    *************************************************
    1.Design the front view of Food menu and Fix the Title. [Done]
    - wp-content\plugins\wp-food-manager\assets\css\frontend.min.css
    - wp-content\plugins\wp-food-manager\templates\content-single-food_manager_menu.php

--- templates/content-single-food_manager_menu.php
+++ templates/content-single-food_manager_menu.php
@@ -33,7 +33,7 @@ if(isset($featured_img_url) && empty($featured_img_url)){
                             <?php the_content(); ?>
                             
                             <h3>
-                                <?php _e('Food Lists'); 
+                                <?php the_title();
                                 $wpfm_radio_icons = get_post_meta(get_the_ID(), 'wpfm_radio_icons', true);
                                 if($wpfm_radio_icons){
                                     echo "<span class='wpfm-front-radio-icon'><i class='fa ".$wpfm_radio_icons."'></i></span>";
@@ -71,8 +71,41 @@ if(isset($featured_img_url) && empty($featured_img_url)){
                                     'orderby'   => 'post__in',
                                 ) );
                                 
-                                foreach ($food_listings as $food_listing) {    
-                                    echo wp_kses_post("<a href='".get_permalink($food_listing->ID)."' class='food-list-box'>".esc_html($food_listing->post_title)."</a>");
+                                foreach ($food_listings as $food_listing) {
+                                    $price_decimals = wpfm_get_price_decimals();
+                                    $price_format = get_food_manager_price_format();
+                                    $price_thousand_separator = wpfm_get_price_thousand_separator();
+                                    $price_decimal_separator = wpfm_get_price_decimal_separator();
+
+
+                                    $sale_price = get_post_meta($food_listing->ID, '_food_sale_price', true);
+                                    $regular_price = get_post_meta($food_listing->ID, '_food_price', true);
+
+                                    if(!empty($sale_price)){
+                                        $formatted_sale_price = number_format($sale_price, $price_decimals, $price_decimal_separator, $price_thousand_separator);
+                                    }
+                                    if(!empty($regular_price)){
+                                        $formatted_regular_price = number_format($regular_price, $price_decimals, $price_decimal_separator, $price_thousand_separator);
+                                    }
+                                    echo "<div class='fm-food-menu-container'>";
+                                        echo wp_kses_post("<a href='".get_permalink($food_listing->ID)."' class='food-list-box'><span class='fm-food-menu-title'><strong>".esc_html($food_listing->post_title))."</strong><i class='fm-food-menu-desc'>".$food_listing->post_content."</i></span>";
+                                            //echo "<span class='fm-divider'> - - - - - - </span>";
+                                            echo "<span class='fm-food-menu-pricing'>";
+                                            if(!empty($regular_price) && !empty($sale_price)){
+                                                $f_regular_price = sprintf($price_format, '<span class="food-manager-Price-currencySymbol">'.get_food_manager_currency_symbol().'</span>', $formatted_sale_price);
+                                                $f_sale_price = sprintf($price_format, '<span class="food-manager-Price-currencySymbol">'.get_food_manager_currency_symbol().'</span>', $formatted_regular_price);
+                                                echo "<del> ".$f_sale_price."</del> <ins><span class='food-manager-Price-currencySymbol'><strong>".$f_regular_price."</strong></ins>"; 
+                                            }
+                                            if(empty($regular_price) && empty($sale_price)){
+                                                return false;
+                                            }
+                                            if(empty($sale_price)){
+                                                echo "<span class='food-manager-Price-currencySymbol'>".get_food_manager_currency_symbol()."</span>".$formatted_regular_price;
+                                            }
+                                            echo "</span>";
+                                        echo "</a>";
+                                    echo "</div>";
+
                                 }
                             }
                             ?>
