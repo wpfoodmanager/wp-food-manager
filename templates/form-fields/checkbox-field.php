bb3112f155d16aa51927ee3b2ec63d5524c6603e
Kushang Tailor <kushangdarji78@gmail.com>
2022-10-20 18:36:56 +0530

    #93 Fix the checkbox issue for frontend.
    Issue: 93
    **********
    Field editor - Checkbox type - Details page not display value
    
    Issue: 93 : Done
    ******************
    1.Fix the selected checkbox issue not working at frontend. [Done]
    wp-content\plugins\wp-food-manager\templates\form-fields\checkbox-field.php

--- templates/form-fields/checkbox-field.php
+++ templates/form-fields/checkbox-field.php
@@ -3,6 +3,6 @@
 	?>
 	<!-- <input type="checkbox" class="input-checkbox" name="<?php echo esc_attr( isset( $field['name'] ) ? $field['name'] : $key ); ?>" id="<?php echo esc_attr( $key ); ?>" <?php if(! empty($field['value']) && $field['value'] == true ){ echo 'checked="checked"'; } ?> value="1" attribute="<?php echo esc_attr( isset( $field['attribute'] ) ? $field['attribute'] : '' ); ?>"  <?php if ( ! empty( $field['required'] ) ) echo 'required'; ?> /> -->
 
-	<label><input type="checkbox" class="input-checkbox" name="<?php echo esc_attr( isset( $field['name'] ) ? $field['name'] : $key ); ?>" id="<?php echo esc_attr( $key ); ?>" attribute="<?php echo esc_attr( isset( $field['attribute'] ) ? $field['attribute'] : '' ); ?>" value="<?php echo esc_attr( $option_key ); ?>" <?php if(isset($field['value']) == $value ){ echo 'checked="checked"'; } ?> /> <?php echo esc_html( $value ); ?></label>
+	<label><input type="checkbox" class="input-checkbox" name="<?php echo esc_attr( isset( $field['name'] ) ? $field['name'] : $key ); ?>[]" id="<?php echo esc_attr( $option_key ); ?>" attribute="<?php echo esc_attr( isset( $field['attribute'] ) ? $field['attribute'] : '' ); ?>" value="<?php echo esc_attr( $option_key ); ?>" <?php if (!empty($field['value']) && is_array($field['value'])) checked(in_array($option_key, $field['value'], true)); ?> /> <?php echo esc_html( $value ); ?></label>
 <?php endforeach; ?>
 <?php if ( ! empty( $field['description'] ) ) : ?><small class="description"><?php echo $field['description']; ?></small><?php endif; ?>
