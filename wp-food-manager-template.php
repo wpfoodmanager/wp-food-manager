ef1bb5fd2363a94158b0b7ecda28c76cc49ae814
Kushang Tailor <kushangdarji78@gmail.com>
2022-10-20 10:34:40 +0530

    #100 Fix & Setup the icons for food types.
    Issue: 100
    ***********
    Food Types can able to set icon same like menu icon
    
    Issue: 100 : Done
    ******************
    1.Fix Food title length issue and Food type icon size issue. [Done]
    - wp-content\plugins\wp-food-manager\assets\css\backend.css
    
    2.Add css for 'Vegan' Food type. [Done]
    - wp-content\plugins\wp-food-manager\assets\css\frontend.min.css
    
    3.Add new image for 'Vegan' food type. [Done]
    - wp-content\plugins\wp-food-manager\assets\images\wpfm-vegan-organic.png
    
    4.Update and modify the code for all 3 food types and their icons. [Done]
    - wp-content\plugins\wp-food-manager\templates\food-dashboard.php
    - wp-content\plugins\wp-food-manager\wp-food-manager-template.php

--- wp-food-manager-template.php
+++ wp-food-manager-template.php
@@ -471,16 +471,25 @@ function display_food_veg_nonveg_icon_tag( $post = null, $after = '' ) {
 
 	$wpfm_veg_nonveg_tags = get_food_veg_nonveg_icon_tag($post);
 
+	$image_id = get_term_meta ( $wpfm_veg_nonveg_tags[0]->term_id, 'image_id', true );
+    $image_src = wp_get_attachment_image_src ( $image_id );
 
     if(!empty($wpfm_veg_nonveg_tags)){
     	foreach($wpfm_veg_nonveg_tags as $wpfm_veg_nonveg_tag){
 			$imagePath = '';
-	        if($wpfm_veg_nonveg_tag->slug === 'vegeterian'){
-	            $imagePath = WPFM_PLUGIN_URL."/assets/images/wpfm-veg-organic.png";
-	        }
-	        if($wpfm_veg_nonveg_tag->slug === 'non-vegeterian'){
-	            $imagePath = WPFM_PLUGIN_URL."/assets/images/wpfm-non-veg-organic.png";
-	        }
+    		if(empty($image_src)){
+		        if($wpfm_veg_nonveg_tag->slug === 'vegeterian'){
+		            $imagePath = WPFM_PLUGIN_URL."/assets/images/wpfm-veg-organic.png";
+		        }
+		        if($wpfm_veg_nonveg_tag->slug === 'non-vegeterian'){
+		            $imagePath = WPFM_PLUGIN_URL."/assets/images/wpfm-non-veg-organic.png";
+		        }
+		        if($wpfm_veg_nonveg_tag->slug === 'vegan'){
+		            $imagePath = WPFM_PLUGIN_URL."/assets/images/wpfm-vegan-organic.png";
+		        }
+		    } else {
+		    	$imagePath = $image_src[0];
+		    }
 	        if(!empty($imagePath)){
 	        	echo '<div class="parent-organic-tag '.$wpfm_veg_nonveg_tag->slug.'"><img alt="'.$wpfm_veg_nonveg_tag->slug.'" src="'.$imagePath.'" class="wpfm-organic-tag-icon '.$wpfm_veg_nonveg_tag->slug.'"></div>';
 	        }
