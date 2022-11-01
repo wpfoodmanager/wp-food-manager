e70f9814cc724b5e38c9d49889929170aef87eed
Kushang Tailor <kushangdarji78@gmail.com>
2022-11-01 10:47:42 +0530

    #131 Hide the 'Choose category' option from the frontend.
    Issue: 131
    ***********
    Food menu - Each category has possibility to assign image
    
    Issue: 131 : Done
    ******************
    1.Fix the warning issues if no category selected and add some validation on it.
    - wp-content\plugins\wp-food-manager\templates\content-single-food_manager_menu.php
    
    2.Hide the 'Choose a category' custom option from the food listing page at frontend and Set only for admin side. [Done]
    - wp-content\plugins\wp-food-manager\wp-food-manager-functions.php

--- wp-food-manager-functions.php
+++ wp-food-manager-functions.php
@@ -866,8 +866,10 @@ function food_manager_dropdown_selection( $args = '' ) {
 	
 	$output = "<select name='" . esc_attr( $name ) . "[]' id='" . esc_attr( $id ) . "' class='" . esc_attr( $class ) . "' " . ( $multiple ? "multiple='multiple'" : '' ) . " data-placeholder='" . esc_attr( $placeholder ) . "' data-no_results_text='" . esc_attr( $no_results_text ) . "' data-multiple_text='" . esc_attr( $multiple_text ) . "'>\n";
 
-	if(empty($item_cat_ids) && isset($item_cat_ids)){
-		$output .= '<option value="" disabled selected>Choose a category</option>';
+	if(is_admin()){
+		if(empty($item_cat_ids) && isset($item_cat_ids)){
+			$output .= '<option value="" disabled selected>Choose a category</option>';
+		}
 	}
 	if ( $show_option_all ) {
 		
