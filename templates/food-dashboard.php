3fb1582ef64685a66c7cd28e5628948e7fc4ef94
Kushang Tailor <kushangdarji78@gmail.com>
2022-11-01 12:34:34 +0530

    #135 Check settings with plain permalink settings.
    Issue: 135
    ***********
    Set Plain option in the Permalink Settings issue
    
    Issue: 135 : Done
    ******************
    1.Check and verify the filter option with Plain permalink settings on local and staging environment. [Done]

--- templates/food-dashboard.php
+++ templates/food-dashboard.php
@@ -25,8 +25,8 @@
 
 		<?php
 		$_GET = array_map('stripslashes_deep', $_GET);
-		$search_keywords = isset($_GET['search_keywords']) ? sanitize_text_field($_GET['search_keywords']) : '';
-		$search_order_by = isset($_GET['search_order_by']) ? sanitize_text_field($_GET['search_order_by']) : '';
+		$search_keywords  = isset($_GET['search_keywords']) ? sanitize_text_field($_GET['search_keywords']) : '';
+		$search_order_by  = isset($_GET['search_order_by']) ? sanitize_text_field($_GET['search_order_by']) : '';
 
 		$display_block = '';
 		if (!empty($search_keywords) || !empty($search_order_by)) {
