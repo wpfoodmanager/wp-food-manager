aba889d27aee3fc74f1b5a7af9025f3738a35522
Kushang Tailor <kushangdarji78@gmail.com>
2022-11-11 17:30:21 +0530

    #154 Display the saved value.
    1.Fix the saved checkbox value not display. [Done]
    - wp-content\plugins\wp-food-manager\templates\form-fields\checkbox-field.php

--- templates/form-fields/checkbox-field.php
+++ templates/form-fields/checkbox-field.php
@@ -10,6 +10,6 @@ if(!empty($field['value']) && is_array($field['value']) && isset($field['value']
 foreach ( $field['options'] as $option_key => $value ) : 
 
 	?>
-	<label><input type="checkbox" class="input-checkbox" name="<?php echo esc_attr( isset( $field['name'] ) ? $field['name'] : $key ); ?>[]" id="<?php echo esc_attr( $option_key ); ?>" attribute="<?php echo esc_attr( isset( $field['attribute'] ) ? $field['attribute'] : '' ); ?>" value="<?php echo esc_attr( $option_key ); ?>" <?php if (!empty($field_val_num) && is_array($field_val_num)) checked(in_array($option_key, $field_val_num, true)); ?> /> <?php echo esc_html( $value ); ?></label>
+	<label><input type="checkbox" class="input-checkbox" name="<?php echo esc_attr( isset( $field['name'] ) ? $field['name'] : $key ); ?>[]" id="<?php echo esc_attr( $option_key ); ?>" attribute="<?php echo esc_attr( isset( $field['attribute'] ) ? $field['attribute'] : '' ); ?>" value="<?php echo esc_attr( $option_key ); ?>" <?php if (!empty($field['value']) && is_array($field['value'])) checked(in_array($option_key, $field['value'], true)); ?> /> <?php echo esc_html( $value ); ?></label>
 <?php endforeach; ?>
 <?php if ( ! empty( $field['description'] ) ) : ?><small class="description"><?php echo $field['description']; ?></small><?php endif; ?>
