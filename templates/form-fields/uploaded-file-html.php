042375d2494a2bbf8685e83a1d26ff233872e7a0
Kushang Tailor <kushangdarji78@gmail.com>
2022-11-17 10:56:53 +0530

    #158 Fix the empty image selection issue.
    Issue: #158
    ************
    Extra Toppings - Image file type is not display properly.
    
    Issue: #158 : Done
    *******************
    1.Fix the empty image selection issue. [Done]
    - wp-content\plugins\wp-food-manager\templates\form-fields\file-field.php
    - wp-content\plugins\wp-food-manager\templates\form-fields\uploaded-file-html.php
    - wp-content\plugins\wp-food-manager\assets\js\ajax-file-upload.min.js
    - wp-content\plugins\wp-food-manager\assets\css\frontend.min.css

--- templates/form-fields/uploaded-file-html.php
+++ templates/form-fields/uploaded-file-html.php
@@ -6,14 +6,13 @@ if ( is_numeric( $value ) ) {
 	$image_src = $value;
 }
 
-//if(!empty($image_src)){ ?>
-	<div class="food-manager-uploaded-file">
-		<?php $extension = ! empty( $extension ) ? $extension : substr( strrchr( $image_src, '.' ), 1 );
-		if ( 3 !== strlen( $extension ) || in_array( $extension, array( 'jpg', 'gif', 'png', 'jpeg', 'jpe' ) ) ) : ?>
-			<span class="food-manager-uploaded-file-preview"><img src="<?php echo esc_url( $image_src ); ?>" /> <a class="food-manager-remove-uploaded-file" href="#">[<?php _e( 'remove', 'wp-food-manager' ); ?>]</a></span>
-		<?php else : ?>
-			<span class="food-manager-uploaded-file-name"><code><?php echo esc_html( basename( $image_src ) ); ?></code> <a class="food-manager-remove-uploaded-file" href="#">[<?php _e( 'remove', 'wp-food-manager' ); ?>]</a></span>
-		<?php endif; ?>
-		<input type="hidden" class="input-text" name="<?php echo esc_attr( $name ); ?>" value="<?php echo esc_attr( $value ); ?>" />
-	</div>
-<?php //} ?>
+$image_src_class = !empty($image_src) ? '' : 'empty-src'; ?>
+<div class="food-manager-uploaded-file <?php echo $image_src_class;?>">
+	<?php $extension = ! empty( $extension ) ? $extension : substr( strrchr( $image_src, '.' ), 1 );
+	if ( 3 !== strlen( $extension ) || in_array( $extension, array( 'jpg', 'gif', 'png', 'jpeg', 'jpe' ) ) ) : ?>
+		<span class="food-manager-uploaded-file-preview"><img src="<?php echo esc_url( $image_src ); ?>" /> <a class="food-manager-remove-uploaded-file" href="#">[<?php _e( 'remove', 'wp-food-manager' ); ?>]</a></span>
+	<?php else : ?>
+		<span class="food-manager-uploaded-file-name"><code><?php echo esc_html( basename( $image_src ) ); ?></code> <a class="food-manager-remove-uploaded-file" href="#">[<?php _e( 'remove', 'wp-food-manager' ); ?>]</a></span>
+	<?php endif; ?>
+	<input type="hidden" class="input-text" name="<?php echo esc_attr( $name ); ?>" value="<?php echo esc_attr( $value ); ?>" />
+</div>
