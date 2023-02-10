4330a201b31c70338ef9eaa868b3268c78479672
wp-kiva <vrykvl67@gmail.com>
2023-02-10 12:44:01 +0530

    Wrong redirection link in Setup Done page. #226

--- admin/wpfm-setup.php
+++ admin/wpfm-setup.php
@@ -428,7 +428,7 @@ class WP_Food_Manager_Setup
 										<div class="wpfm-setup-help-center-block-content">
 											<div class="wpfm-setup-help-center-block-heading"><?php esc_attr_e('Knowledge Base', 'wp-food-manager'); ?></div>
 											<div class="wpfm-setup-help-center-block-desc"><?php esc_attr_e('Solve your queries by browsing our documentation.', 'wp-food-manager'); ?></div>
-											<a href="https://wpfoodmanager.com/" target="_blank" class="wpfm-setup-help-center-block-link"><span class="wpfm-setup-help-center-box-target-text"><?php esc_attr_e('Browse More', 'wp-food-manager'); ?> »</span></a>
+											<a href="https://wpfoodmanager.com/knowledge-base/" target="_blank" class="wpfm-setup-help-center-block-link"><span class="wpfm-setup-help-center-box-target-text"><?php esc_attr_e('Browse More', 'wp-food-manager'); ?> »</span></a>
 										</div>
 									</div>
 									<div class="wpfm-setup-help-center-block">
@@ -438,7 +438,7 @@ class WP_Food_Manager_Setup
 										<div class="wpfm-setup-help-center-block-content">
 											<div class="wpfm-setup-help-center-block-heading"><?php esc_attr_e('FAQs', 'wp-food-manager'); ?></div>
 											<div class="wpfm-setup-help-center-block-desc"><?php esc_attr_e('Explore through the frequently asked questions.', 'wp-food-manager'); ?></div>
-											<a href="https://wpfoodmanager.com/" target="_blank" class="wpfm-setup-help-center-block-link"><span class="wpfm-setup-help-center-box-target-text"><?php esc_attr_e('Get Answers', 'wp-food-manager'); ?> »</span></a>
+											<a href="https://wpfoodmanager.com/faqs/" target="_blank" class="wpfm-setup-help-center-block-link"><span class="wpfm-setup-help-center-box-target-text"><?php esc_attr_e('Get Answers', 'wp-food-manager'); ?> »</span></a>
 										</div>
 									</div>
 									<div class="wpfm-setup-help-center-block">
@@ -460,7 +460,7 @@ class WP_Food_Manager_Setup
 										<div class="wpfm-setup-help-center-block-content">
 											<div class="wpfm-setup-help-center-block-heading"><?php esc_attr_e('Add ons Support', 'wp-food-manager'); ?></div>
 											<div class="wpfm-setup-help-center-block-desc"><?php esc_attr_e('Get support for all the Add ons related queries with our experienced/ talented support team.', 'wp-food-manager'); ?></div>
-											<a href="https://wpfoodmanager.com/" target="_blank" class="wpfm-setup-help-center-block-link"><span class="wpfm-setup-help-center-box-target-text"><?php esc_attr_e('Get Add ons Support', 'wp-food-manager'); ?> »</span></a>
+											<a href="https://wpfoodmanager.com/help-center/" target="_blank" class="wpfm-setup-help-center-block-link"><span class="wpfm-setup-help-center-box-target-text"><?php esc_attr_e('Get Add ons Support', 'wp-food-manager'); ?> »</span></a>
 										</div>
 									</div>
 								</div>
