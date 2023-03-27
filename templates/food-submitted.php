ca2eaafff369b9c04386adc420b1a935acf26af8
variya keval <vrykvl67@gmail.com>
2023-03-27 11:06:18 +0530

    Fronted : Notice text improvement. #387

--- templates/food-submitted.php
+++ templates/food-submitted.php
@@ -5,7 +5,7 @@ switch ($food->post_status):
 		printf('<p class="post-submitted-success-green-message wpfm-alert wpfm-alert-success">' . __('%s listed successfully. To view your listing <a href="%s">click here</a>.', 'wp-food-manager') . '</p>', $wp_post_types['food_manager']->labels->singular_name, get_permalink($food->ID));
 		break;
 	case 'pending':
-		printf('<p class="post-submitted-success-green-message wpfm-alert wpfm-alert-success">' . __('Your %s added successfully in Food Manager. Your listing will be visible once approved by admin.', 'wp-food-manager') . '</p>', $wp_post_types['food_manager']->labels->singular_name, get_permalink($food->ID));
+		printf('<p class="post-submitted-success-green-message wpfm-alert wpfm-alert-success">' . __("Your %s has been successfully added to WP Event Manager. Your food listing will be visible after the admin's approval.", 'wp-food-manager') . '</p>', $wp_post_types['food_manager']->labels->singular_name, get_permalink($food->ID));
 		break;
 	default:
 		do_action('food_manager_food_submitted_content_' . str_replace('-', '_', sanitize_title($food->post_status)), $food);
