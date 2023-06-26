94a9fd7367683fdc487fbf90718dc30edc759629
wp-kiva <119648390+vry-kvl@users.noreply.github.com>
2023-06-26 16:59:35 +0530

    Admin : Advanced setting switch is not works properly #470

--- templates/form-fields/switch-field.php
+++ templates/form-fields/switch-field.php
@@ -3,8 +3,9 @@ global $thepostid;
 $name = (!empty($field['name'])) ? esc_attr($field['name']) : esc_attr($key);
 $food_id = (isset($_GET['food_id']) && !empty($_GET['food_id'])) ? esc_attr($_GET['food_id']) : esc_attr($thepostid);
 $exp_arr = explode("_", $key);
+$value = get_post_meta($food_id, '_' . $key, true);
 ?>
 <label class="wpfm-field-switch" for="<?php echo esc_attr($key); ?>">
-    <input type="checkbox" id="<?php echo esc_attr($key); ?>" name="<?php echo $name; ?>" value="1" <?php echo (!empty($field['value']) && $field['value'] == 1) ? 'checked' : ''; ?>>
+    <input type="checkbox" id="<?php echo esc_attr($key); ?>" name="<?php echo $name; ?>" value="1" <?php echo (!empty($value) && $value == 1) ? 'checked' : ''; ?>>
     <span class="wpfm-field-switch-slider round"></span>
 </label>
