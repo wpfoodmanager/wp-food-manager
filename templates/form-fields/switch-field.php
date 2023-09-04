a7f43a6a8f7af7e3c189557da9a78ce99e7de072
variya keval <136054608+dhruvikathrotiya@users.noreply.github.com>
2023-09-04 16:10:01 +0530

    #498
    add/edit food from the front side warning in the debug log and Error in the advanced section are resolved.

--- templates/form-fields/switch-field.php
+++ templates/form-fields/switch-field.php
@@ -2,8 +2,9 @@
 global $thepostid;
 $name = (!empty($field['name'])) ? esc_attr($field['name']) : esc_attr($key);
 $food_id = (isset($_GET['food_id']) && !empty($_GET['food_id'])) ? esc_attr($_GET['food_id']) : esc_attr($thepostid);
-$exp_arr = explode("_", $key);
 $value = get_post_meta($food_id, '_' . $key, true);
+$exp_arr = explode("_", $key);
+
 ?>
 <label class="wpfm-field-switch" for="<?php echo esc_attr($key); ?>">
     <input type="checkbox" id="<?php echo esc_attr($key); ?>" name="<?php echo $name; ?>" value="1" <?php echo (!empty($value) && $value == 1) ? 'checked' : ''; ?>>
