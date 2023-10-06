ca7f7570cd2b663f159930abafc4eaab1b098ade
Hiren <sangani.reeta@gmail.com>
2023-10-06 16:35:23 +0530

    Code cleanup #62

--- templates/form-fields/timezone-field.php
+++
@@ -1,6 +0,0 @@
-<select name="<?php echo esc_attr(isset($field['name']) ? $field['name'] : $key); ?>" id="<?php echo isset($field['id']) ? esc_attr($field['id']) : esc_attr($key); ?>" class="input-select <?php echo esc_attr(isset($field['class']) ? $field['class'] : $key); ?>">
-	<?php
-	$value = isset($field['value']) ? $field['value'] : $field['default'];
-	echo esc_html(WPFM_Date_Time::timezone_choice($value));
-	?>
-</select>
