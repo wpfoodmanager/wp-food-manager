3c44a103b1d13802964737ca3bb01b0351d5d9e8
Hiren <sangani.reeta@gmail.com>
2024-06-25 14:43:53 +0530

    Revert "Merge branch '1_0_6' of https://github.com/wpfoodmanager/wp-food-manager into 1_0_6"
    This reverts commit 72f28f997c3cfefba830bc9ddaf12d5b16a113a6, reversing
    changes made to f45f35ecabe3e6c0229608d0e8e78bb68db36869.

--- includes/wpfm-action-hooks.php
+++ includes/wpfm-action-hooks.php
@@ -326,10 +326,7 @@ class WPFM_ActionHooks {
         }
         
         wp_enqueue_style('chosen', esc_url(WPFM_PLUGIN_URL . '/assets/css/chosen.min.css'));
-
-        // Enqueue the script for search food menu.
-        wp_register_script('food-menu-search', esc_url(WPFM_PLUGIN_URL . '/assets/js/food-menu-search.js'), array('jquery'), WPFM_VERSION, true);
-
+        
         // File upload - vendor.
         // if (apply_filters('wpfm_ajax_file_upload_enabled', true)) {
             wp_register_script('jquery-iframe-transport', esc_url(WPFM_PLUGIN_URL . '/assets/js/jquery-fileupload/jquery.iframe-transport.min.js'), array('jquery'), '1.8.3', true);
@@ -350,7 +347,7 @@ class WPFM_ActionHooks {
         // }
         
         // Frontend Css.
-        wp_enqueue_style('wpfm-frontend', esc_url(WPFM_PLUGIN_URL . '/assets/css/frontend.css'));
+        wp_enqueue_style('wpfm-frontend', esc_url(WPFM_PLUGIN_URL . '/assets/css/frontend.min.css'));
 
         // Frontend js.
         wp_register_script('wp-food-manager-frontend', esc_url(WPFM_PLUGIN_URL . '/assets/js/frontend.min.js'), array('jquery'), WPFM_VERSION, true);
