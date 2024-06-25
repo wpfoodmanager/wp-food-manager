a6dad4fe2d7fd178d7953993235b4479d0f8f657
Hiren <sangani.reeta@gmail.com>
2024-06-25 15:00:46 +0530

    Provide search option in menu. #578

--- includes/wpfm-action-hooks.php
+++ includes/wpfm-action-hooks.php
@@ -347,7 +347,10 @@ class WPFM_ActionHooks {
         // }
         
         // Frontend Css.
-        wp_enqueue_style('wpfm-frontend', esc_url(WPFM_PLUGIN_URL . '/assets/css/frontend.min.css'));
+        wp_enqueue_style('wpfm-frontend', esc_url(WPFM_PLUGIN_URL . '/assets/css/frontend.css'));
+
+        // Enqueue the script for search food menu.
+        wp_register_script('food-menu-search', esc_url(WPFM_PLUGIN_URL . '/assets/js/food-menu-search.js'), array('jquery'), WPFM_VERSION, true);
 
         // Frontend js.
         wp_register_script('wp-food-manager-frontend', esc_url(WPFM_PLUGIN_URL . '/assets/js/frontend.min.js'), array('jquery'), WPFM_VERSION, true);
---
+++ templates/content-food-menu.php
@@ -0,0 +1,13 @@
+<!-- search box for search food menu -->
+<input type="text" id="food-menu-search" placeholder="Search for food items..." style="margin-bottom: 20px; padding: 5px; width: 100%; box-sizing: border-box;">
+<div id="food-menu-container">
+    <!-- Food menu heading  -->
+    <?php if ($title_query->have_posts()) : ?>
+        <div class="food-menu-titles" id="food-menu-titles" style="display: flex; flex-wrap: wrap; gap: 10px;">
+            <?php while ($title_query->have_posts()) : $title_query->the_post(); ?>
+                <div class="wpfm-food-menu-title" style="flex: 0 auto;">
+                    <a href="#menu-<?php the_ID(); ?>" class="wpfm-menu-title-link"><?php the_title(); ?></a>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    <?php endif;
