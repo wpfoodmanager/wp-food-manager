09c3cb4f1d72c33a329b7c5eb0f0d640443a015c
kaushik <wpem.kaushik.pokal@gmail.com>
2024-06-26 12:47:01 +0530

    #578

--- templates/content-food-menu.php
+++ templates/content-food-menu.php
@@ -1,13 +1,24 @@
-<!-- search box for search food menu -->
-<input type="text" id="food-menu-search" placeholder="Search for food items..." style="margin-bottom: 20px; padding: 5px; width: 100%; box-sizing: border-box;">
-<div id="food-menu-container">
-    <!-- Food menu heading  -->
-    <?php if ($title_query->have_posts()) : ?>
-        <div class="food-menu-titles" id="food-menu-titles" style="display: flex; flex-wrap: wrap; gap: 10px;">
-            <?php while ($title_query->have_posts()) : $title_query->the_post(); ?>
-                <div class="wpfm-food-menu-title" style="flex: 0 auto;">
-                    <a href="#menu-<?php the_ID(); ?>" class="wpfm-menu-title-link"><?php the_title(); ?></a>
+
+
+<div id="food-menu-container" class="wpfm-food-menu-page-main-container">
+     <div class="food-menu-page-filters">
+        <!-- search box for search food menu -->
+        <div class="wpfm-form-wrapper">
+            <div class="wpfm-form-group">
+                <input type="text" id="food-menu-search" placeholder="Search for food items...">
+            </div>
+        </div>
+    
+        <!-- Food menu tabs  -->
+        <?php if ($title_query->have_posts()) : ?>
+            <div class="food-menu-page-filter-tabs-wrapper">
+                <div class="food-menu-page-filter-tabs" id="food-menu-titles">
+                    <?php while ($title_query->have_posts()) : $title_query->the_post(); ?>
+                        <div class="food-menu-page-filter-tab">
+                            <a href="#menu-<?php the_ID(); ?>" class="food-menu-page-filter-tab-link"><?php the_title(); ?></a>
+                        </div>
+                    <?php endwhile; ?>
                 </div>
-            <?php endwhile; ?>
+            </div>
         </div>
     <?php endif;
