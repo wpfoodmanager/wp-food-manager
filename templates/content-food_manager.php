ddbe84d5dc4a4172585be27624786a82b80275bf
Dhruvi Kathrotiya <dhruvikathrotiya19@gmail.com>
2024-10-09 13:37:41 +0900

    #646
    Need to bind the hook for the food favorites list addon.

--- templates/content-food_manager.php
+++ templates/content-food_manager.php
@@ -48,6 +48,7 @@ if (get_option('food_manager_food_item_show_hide') == 0 && get_stock_status() !=
                         <a href="<?php display_food_permalink(); ?>"
                             class="wpfm-food-action-url food-style-color <?php echo esc_attr($food_type); ?>">
                             <div class="wpfm-food-title">
+                                <?php do_action('food_list_title_after', get_the_ID()); ?>
                                 <h3 class="wpfm-heading-text">
                                     <?php
                                     $out = strlen(get_the_title()) > 50 ? substr(get_the_title(), 0, 50) . "..." : get_the_title();
@@ -56,7 +57,6 @@ if (get_option('food_manager_food_item_show_hide') == 0 && get_stock_status() !=
                                 </h3>
                             </div>
                         </a>
-                        <?php do_action('food_list_title_after', get_the_ID()); ?>
                         <div class="wpfm-food-pricing-box">
                             <div class="wpfm-food-price">
                                 <?php display_food_price_tag(); ?>
