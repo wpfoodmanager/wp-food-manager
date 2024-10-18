4729885970f0d31683fb09b5c75e7cf52da5a4e5
Rashmi Singh <93907749+rashmipsingh@users.noreply.github.com>
2024-10-18 10:51:46 +0530

    #625
    Fix food page topping button position

--- admin/templates/food-data-toppings.php
+++ admin/templates/food-data-toppings.php
@@ -70,8 +70,67 @@ $food_toppings = get_post_meta($thepostid, '_food_toppings', true);
 					</div>
 			<?php
 				}
-			}
+			} else { ?>
+				<div class="wpfm-options-wrap wpfm-metabox postbox wpfm-options-box-1">
+					<input type="hidden" name="repeated_options[]" value="1" class="repeated-options">
+					<h3 class="">
+						<a href="javascript: void(0);" data-id="1" class="wpfm-delete-btn">Remove</a>
+						<div class="wpfm-togglediv" title="Click to toggle" aria-expanded="false" data-row-count="1"></div>
+						<div class="wpfm-sort"></div>
+						<strong class="attribute_name"><?php _e("Option 1", "wp-food-manager"); ?></strong>
+					<span class="attribute_key"><input type="hidden" name="topping_key1"
+							value="option1" readonly>
+					</span>
+				</h3>
+				<div class="wpfm-metabox-content wpfm-options-box">
+					<div class="wpfm-content">
+						<?php
+						do_action("food_manager_food_data_start", $thepostid);
+						$topping_fields = $this->food_manager_data_fields();
+						if (isset($topping_fields["toppings"]))
+							foreach ($topping_fields["toppings"] as $key => $field) {
+
+								$field['required'] = false;
+								if ($key == "_topping_name") {
+									if (strpos($key, '_') !== 0) {
+										$key = $key . '___repeated-option-index__';
+									}
+								} else {
+									if (strpos($key, '_') !== 0) {
+										$key = $key . "___repeated-option-index__";
+									}
+								}
+
+								$type = !empty($field["type"]) ? $field["type"] : "text";
+								if ($type == "wp-editor")
+									$type = "wp_editor";
+								if ($type == "term-autocomplete")
+									$type = "term_autocomplete";
+								?>
+								<p class="wpfm-admin-postbox-form-field <?php echo esc_attr($key);
+								echo ($type == "wp_editor") ? ' wp-editor-field' : ''; ?>" <?php echo ($type == "wp_editor") ? 'data-field-name="' . esc_attr($key) . '"' : ''; ?>>
+									<label for="<?php echo esc_attr($key); ?>"><?php echo esc_html($field['label']); ?> : </label>
+									<!-- Adding the notice -->
+									<?php if ($field['label'] === "Options"): ?>
+									<div class="wpfm-topping-option-notice">
+										<?php _e("Please provide the options and price for the topping."); ?>
+									</div>
+								<?php endif; ?>
+								<?php if ($type != 'options')
+									echo '<span class="wpfm-input-field">'; ?>
+								<?php get_food_manager_template('form-fields/' . $field['type'] . '-field.php', array('key' => esc_attr($key), 'field' => $field)); ?>
+								<?php if ($type != 'options')
+									echo '</span>'; ?>
+								</p>
+								<?php
+							}
+						do_action("food_manager_food_data_end", $thepostid); ?>
+					</div>
+				</div>
+			</div>
+			<?php }
 			?>
+			
 			<div class="wpfm-actions">
 				<button type="button" class="wpfm-add-button button button-primary" id="wpfm-add-new-option" data-row='<div class="wpfm-options-wrap wpfm-metabox postbox wpfm-options-box-__repeated-option-index__">
 					<input type="hidden" name="repeated_options[]" value="__repeated-option-index__" class="repeated-options">
