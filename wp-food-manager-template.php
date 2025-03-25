e821f4cc7288a8d478207267f222a5615bb6677f
priyansiboghara <132275836+priyansiboghara@users.noreply.github.com>
2025-03-25 12:23:39 +0530

    Revert "#717"
    This reverts commit 5f549d506bba98b2b46ceb6e5aa856ee7548035a.

--- wp-food-manager-template.php
+++ wp-food-manager-template.php
@@ -453,8 +453,8 @@ function display_food_category($post = null, $after = '') {
 					),
 				));
 				if ($numCategory > ++$i) {
-                    echo ', ' . esc_attr($after);  // Add a comma and the 'after' string
-                }
+					echo esc_attr($after);
+				}
 			}
 		}
 	} else {
